4a1dce9f034f672aa3261285c1a5a59e46cff3e4
Behdad <behdad.222@gmail.com>
2020-02-05 20:35:14 +0330

    fix tapsell plus

--- files/tapsellplus/edittime.js
+++ files/tapsellplus/edittime.js
@@ -1,29 +1,28 @@
-﻿function GetPluginSettings()
-{
+﻿function GetPluginSettings() {
 	return {
-		"name":			"TapsellPlus",				// as appears in 'insert object' dialog, can be changed as long as "id" stays the same
-		"id":			"TapsellPlus_Construct2_SDK",				// this is used to identify this plugin and is saved to the project; never change it
-		"version":		"1.0",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
-		"description":	"Tapsell Plus Ads SDK for Construct 2",
-		"author":		"Tapsell",
-		"help url":		"https://docs.tapsell.ir/",
-		"category":		"Ads",				// Prefer to re-use existing categories, but you can set anything here
-		"type":			"object",				// either "world" (appears in layout and is drawn), else "object"
-		"rotatable":	false,					// only used when "type" is "world".  Enables an angle property on the object.
-		"flags":		0						// uncomment lines to enable flags...
-						| pf_singleglobal		// exists project-wide, e.g. mouse, keyboard.  "type" must be "object".
-					//	| pf_texture			// object has a single texture (e.g. tiled background)
-					//	| pf_position_aces		// compare/set/get x, y...
-					//	| pf_size_aces			// compare/set/get width, height...
-					//	| pf_angle_aces			// compare/set/get angle (recommended that "rotatable" be set to true)
-					//	| pf_appearance_aces	// compare/set/get visible, opacity...
-					//	| pf_tiling				// adjusts image editor features to better suit tiled images (e.g. tiled background)
-					//	| pf_animations			// enables the animations system.  See 'Sprite' for usage
-					//	| pf_zorder_aces		// move to top, bottom, layer...
-					//  | pf_nosize				// prevent resizing in the editor
-					//	| pf_effects			// allow WebGL shader effects to be added
-					//  | pf_predraw			// set for any plugin which draws and is not a sprite (i.e. does not simply draw
-												// a single non-tiling image the size of the object) - required for effects to work properly
+		"name": "TapsellPlus",				// as appears in 'insert object' dialog, can be changed as long as "id" stays the same
+		"id": "TapsellPlus",				// this is used to identify this plugin and is saved to the project; never change it
+		"version": "1.0",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
+		"description": "Tapsell Plus Ads SDK for Construct 2",
+		"author": "Tapsell",
+		"help url": "https://docs.tapsell.ir/",
+		"category": "Ads",				// Prefer to re-use existing categories, but you can set anything here
+		"type": "object",				// either "world" (appears in layout and is drawn), else "object"
+		"rotatable": false,					// only used when "type" is "world".  Enables an angle property on the object.
+		"flags": 0						// uncomment lines to enable flags...
+			| pf_singleglobal		// exists project-wide, e.g. mouse, keyboard.  "type" must be "object".
+		//	| pf_texture			// object has a single texture (e.g. tiled background)
+		//	| pf_position_aces		// compare/set/get x, y...
+		//	| pf_size_aces			// compare/set/get width, height...
+		//	| pf_angle_aces			// compare/set/get angle (recommended that "rotatable" be set to true)
+		//	| pf_appearance_aces	// compare/set/get visible, opacity...
+		//	| pf_tiling				// adjusts image editor features to better suit tiled images (e.g. tiled background)
+		//	| pf_animations			// enables the animations system.  See 'Sprite' for usage
+		//	| pf_zorder_aces		// move to top, bottom, layer...
+		//  | pf_nosize				// prevent resizing in the editor
+		//	| pf_effects			// allow WebGL shader effects to be added
+		//  | pf_predraw			// set for any plugin which draws and is not a sprite (i.e. does not simply draw
+		// a single non-tiling image the size of the object) - required for effects to work properly
 	};
 };
 
@@ -53,20 +52,15 @@
 //				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
 //				description,		// appears in event wizard dialog when selected
 //				script_name);		// corresponding runtime function name
-				
-// example				
+
+// example
 AddStringParam("Zone Id", "Id of zone for which ad is available.");
-AddCondition(0, cf_trigger, "Request native response", "Tapsell Plus",
-	"If ad is available for zone {0}", "Native ad is available",
-	"request_native_response");
+AddCondition(1, cf_trigger, "Request response", "Tapsell Plus",
+	"If ad is available for zone {0}", "Ad is available", "requestResponse");
 
 AddStringParam("Zone Id", "Id of zone for which ad is available.");
-AddCondition(1, cf_trigger, "Request response", "Tapsell Plus", 
-	"If ad is available for zone {0}", "Ad is available", "request_response");
-
-AddStringParam("Zone Id", "Id of zone for which ad is available.");
-AddCondition(2, cf_trigger, "Request error", "Tapsell Plus", 
-	"If request returned error for zone {0}", "On rrror", "request_error");
+AddCondition(2, cf_trigger, "Request error", "Tapsell Plus",
+	"If request returned error for zone {0}", "On rrror", "requestError");
 
 ////////////////////////////////////////
 // Actions
@@ -80,16 +74,20 @@ AddCondition(2, cf_trigger, "Request error", "Tapsell Plus",
 //			 script_name);		// corresponding runtime function name
 
 // example
-
-AddStringParam("Zone Id","Id of zone from tapsell dashboard")
+AddStringParam("Zone Id", "Id of zone from tapsell dashboard")
 AddAction(0, af_none, "Request rewarded", "Tapsell Plus",
 	"Request a rewarded ad for zone with id {0}", "Request rewarded ad",
-	"request_rewarded");
+	"requestRewarded");
 
-AddStringParam("Zone Id","Id of zone from tapsell dashboard")
+AddStringParam("Zone Id", "Id of zone from tapsell dashboard")
+AddAction(0, af_none, "Request interstitial", "Tapsell Plus",
+	"Request a interstitial ad for zone with id {0}", "Request interstitial ad",
+	"requestInterstitial");
+
+AddStringParam("Zone Id", "Id of zone from tapsell dashboard")
 AddAction(2, af_none, "Show ad", "Tapsell Plus",
 	"Show an ad for zone with id {0}", "Show ad",
-	"show_ad");
+	"showAd");
 
 ////////////////////////////////////////
 // Expressions
@@ -119,72 +117,59 @@ ACESDone();
 // new cr.Property(ept_link,		name,	link_text,		description, "firstonly")		// has no associated value; simply calls "OnPropertyChanged" on click
 
 var property_list = [
-    new cr.Property(ept_text, "tapsellAppId", "", "")
+	new cr.Property(ept_text, "tapsellAppId", "", "")
 ];
 
 // Called by IDE when a new object type is to be created
-function CreateIDEObjectType()
-{
+function CreateIDEObjectType() {
 	return new IDEObjectType();
 }
 
 // Class representing an object type in the IDE
-function IDEObjectType()
-{
+function IDEObjectType() {
 	assert2(this instanceof arguments.callee, "Constructor called as a function");
 }
 
 // Called by IDE when a new object instance of this type is to be created
-IDEObjectType.prototype.CreateInstance = function(instance)
-{
+IDEObjectType.prototype.CreateInstance = function (instance) {
 	return new IDEInstance(instance);
 }
 
 // Class representing an individual instance of an object in the IDE
-function IDEInstance(instance, type)
-{
+function IDEInstance(instance, type) {
 	assert2(this instanceof arguments.callee, "Constructor called as a function");
-	
+
 	// Save the constructor parameters
 	this.instance = instance;
 	this.type = type;
-	
+
 	// Set the default property values from the property table
 	this.properties = {};
-	
+
 	for (var i = 0; i < property_list.length; i++)
 		this.properties[property_list[i].name] = property_list[i].initial_value;
-		
-	// Plugin-specific variables
-	// this.myValue = 0...
 }
 
 // Called when inserted via Insert Object Dialog for the first time
-IDEInstance.prototype.OnInserted = function()
-{
+IDEInstance.prototype.OnInserted = function () {
 }
 
 // Called when double clicked in layout
-IDEInstance.prototype.OnDoubleClicked = function()
-{
+IDEInstance.prototype.OnDoubleClicked = function () {
 }
 
 // Called after a property has been changed in the properties bar
-IDEInstance.prototype.OnPropertyChanged = function(property_name)
-{
+IDEInstance.prototype.OnPropertyChanged = function (property_name) {
 }
 
 // For rendered objects to load fonts or textures
-IDEInstance.prototype.OnRendererInit = function(renderer)
-{
+IDEInstance.prototype.OnRendererInit = function (renderer) {
 }
 
 // Called to draw self in the editor if a layout object
-IDEInstance.prototype.Draw = function(renderer)
-{
+IDEInstance.prototype.Draw = function (renderer) {
 }
 
 // For rendered objects to release fonts or textures
-IDEInstance.prototype.OnRendererReleased = function(renderer)
-{
+IDEInstance.prototype.OnRendererReleased = function (renderer) {
 }
---
+++ files/tapsellplus/runtime.js
@@ -0,0 +1,179 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.TapsellPlus = function (runtime) {
+	this.runtime = runtime;
+};
+
+(function () {
+
+	/////////////////////////////////////
+	// Tapsell Plus
+	function TapsellPlus(appId) {
+		this.response = null;
+		this.error = null;
+		this.appId = appId;
+
+		tapsellPlus.initialize(appId)
+	};
+
+	TapsellPlus.prototype.requestRewarded = function (zoneId) {
+		tapsellPlus.requestRewarded(zoneId)
+	}
+
+	TapsellPlus.prototype.requestInterstitial = function (zoneId) {
+		tapsellPlus.requestInterstitial(zoneId)
+	}
+
+	TapsellPlus.prototype.showAd = function (zoneId) {
+		tapsellPlus.showAd(zoneId)
+	}
+
+	TapsellPlus.prototype.requestResponse = function (zoneId) {
+		this.requestResponse(zoneId)
+	}
+
+	TapsellPlus.prototype.requestError = function (zoneId, errorMessage) {
+		this.requestError(zoneId, errorMessage)
+	}
+
+	window.requestResponse = function (zoneId) {
+		window.myTapsellPlusInstance.requestResponse(zoneId);
+	}
+
+	window.requestError = function (zoneId, errorMessage) {
+		window.myTapsellPlusInstance.requestError(zoneId, errorMessage);
+	}
+
+
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.TapsellPlus.prototype;
+
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function (plugin) {
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+
+	// called on startup for each object type
+	typeProto.onCreate = function () {
+		if (this.runtime.isBlackberry10 || this.runtime.isWindows8App || this.runtime.isWindowsPhone8 || this.runtime.isWindowsPhone81) {
+			var scripts = document.getElementsByTagName("script");
+			var scriptExist = false;
+			for (var i = 0; i < scripts.length; i++) {
+				if (scripts[i].src.indexOf("cordova.js") != -1 || scripts[i].src.indexOf("phonegap.js") != -1) {
+					scriptExist = true;
+					break;
+				}
+			}
+			if (!scriptExist) {
+				var newScriptTag = document.createElement("script");
+				newScriptTag.setAttribute("type", "text/javascript");
+				newScriptTag.setAttribute("src", "cordova.js");
+				document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+			}
+		}
+	};
+
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function (type) {
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function () {
+		this.appId = this.properties[0];
+		this.tapsellplus_instance = null;
+		this.tapsellplus_instance = new TapsellPlus(this.appId);
+		window.myTapsellPlusInstance = this.tapsellplus_instance;
+
+		var self = this;
+
+		if (this.tapsellplus_instance) {
+			this.requestResponse = null;
+			this.requestError = null;
+
+			this.tapsellplus_instance.requestResponse = function (zone) {
+				self.runtime.trigger(cr.plugins_.TapsellPlus.prototype.cnds.requestResponse, self);
+			};
+			this.tapsellplus_instance.requestError = function () {
+				self.runtime.trigger(cr.plugins_.TapsellPlus.prototype.cnds.requestError, self);
+			};
+		}
+	};
+
+
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function (ctx) {
+	};
+
+
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw) {
+	};
+
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() { };
+
+	Cnds.prototype.requestResponse = function () {
+		return true;
+	};
+
+	Cnds.prototype.requestError = function () {
+		return true;
+	};
+
+	pluginProto.cnds = new Cnds();
+
+
+	//////////////////////////////////////
+	// Actions 
+	function Acts() { };
+
+	Acts.prototype.requestRewarded = function (zoneId) {
+		this.tapsellplus_instance.requestRewarded(zoneId);
+	};
+
+	Acts.prototype.requestInterstitial = function (zoneId) {
+		this.tapsellplus_instance.requestInterstitial(zoneId);
+	};
+
+	Acts.prototype.showAd = function (zoneId) {
+		this.tapsellplus_instance.showAd(zoneId);
+	}
+
+	pluginProto.acts = new Acts();
+
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() { };
+
+	pluginProto.exps = new Exps();
+
+}());
